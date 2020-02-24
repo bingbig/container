50d4146a8de8c6ff20ae3fab858f233cbfd13726
bingbig <598604711@qq.com>
2020-02-24 12:05:26 +0800

    import reexec

--- container.go
+++ container.go
@@ -5,25 +5,60 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+
+	"github.com/docker/docker/pkg/reexec"
 )
 
-func main() {
-	switch os.Args[1] {
-	case "run":
-		run()
-	default:
-		panic("pass me an argument please")
+func init() {
+	reexec.Register("nsInitialisation", nsInitialisation)
+	if reexec.Init() {
+		os.Exit(0)
 	}
 }
-func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+func nsInitialisation() {
+	fmt.Printf("\n>> namespace setup code goes here <<\n\n")
+	nsRun()
+}
+
+func nsRun() {
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	cmd.Env = []string{"PS1=-[container]- # "}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Error running the %s command - %s\n", os.Args[1], err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("pass me an argument please")
+	}
+
+	switch os.Args[1] {
+	case "run":
+		if len(os.Args) < 3 {
+			panic("pass me a cmd to run in container please")
+		}
+		run()
+	default:
+		panic("pass me an argument please")
+	}
+}
+
+func run() {
+	cmd := reexec.Command(append([]string{"nsInitialisation"},
+		os.Args[2:]...)...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWNS |
@@ -49,7 +84,7 @@ func run() {
 	}
 
 	if err := cmd.Run(); err != nil {
-		fmt.Printf("Error running the %s command - %s\n", os.Args[1], err)
+		fmt.Printf("Error running the reexec.Command - %s\n", err)
 		os.Exit(1)
 	}
 }
