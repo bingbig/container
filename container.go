df145e0fa7c78e16f141500c00e4c3978b2a507d
bingbig <598604711@qq.com>
2020-02-24 10:34:15 +0800

    user namespace

--- container.go
+++ container.go
@@ -32,6 +32,20 @@ func run() {
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNET |
 			syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getuid(),
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:      os.Getgid(),
+				Size:        1,
+			},
+		},
 	}
 
 	if err := cmd.Run(); err != nil {
